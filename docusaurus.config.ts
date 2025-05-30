e954318fd89c8438aaacd8aecfe2c8f3ac880e3e
Thomas Thaulow Stöcklin <thaulow@thaulow.co>
2025-05-30 11:37:45 +0200

    add algolia and custom css

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -102,7 +102,7 @@ const config: Config = {
       lastVersion: 'current',
       versions: {
         current: {
-          label: '1.0.0',
+          label: 'latest',
         },
       },
                 editUrl:
@@ -191,6 +191,27 @@ const config: Config = {
       },
       
     },
+    algolia: {
+      // The application ID provided by Algolia
+      appId: 'XNAMH6R6DR',
+
+      // Public API key: it is safe to commit it
+      apiKey: 'a819bc3f325dc2e6048e75474d382383',
+
+      indexName: 'thaulowio',
+
+      // Optional: see doc section below
+      contextualSearch: true,
+
+      // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+     // externalUrlRegex: 'external\\.com|domain\\.com',
+
+      // Optional: Replace parts of the item URLs from Algolia. Useful when using the same search index for multiple deployments using a different baseUrl. You can use regexp or string in the `from` param. For example: localhost:3000 vs myCompany.com/docs
+     // replaceSearchResultPathname: {
+      //  from: '/docs/', // or as RegExp: /\/docs\//
+     //   to: '/',
+      },
+
     announcementBar: {
     id: 'announcement_bar', // A unique id for this bar
     content:
@@ -248,11 +269,9 @@ const config: Config = {
           href: 'https://sky.sigma2.no',
           label: 'Access Sky',
           position: 'right',
+          className: 'navbar-access-sky',
         },
-        {
-          type: 'search',
-          position: 'right',
-        },
+
 
       ],
     },
